49768be60bd02ba8ecd0dbfd18cb5844db6e6072
Yashwanth-raju005 <159526001+Yashwanth-raju005@users.noreply.github.com>
2025-09-20 14:11:57 +0530

    Update Login.jsx

--- FrontEnd/src/landing_page/Logins/Login.jsx
+++ FrontEnd/src/landing_page/Logins/Login.jsx
@@ -15,7 +15,7 @@ const Login = () => {
     e.preventDefault();
 
     try {
-      const res = await axios.post("http://localhost:8080/login", {
+      const res = await axios.post("https://zerodha-clone-me02.onrender.com/login", {
         email,
         password,
       });
